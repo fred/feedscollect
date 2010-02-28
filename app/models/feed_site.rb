de3bfb65006df262cd5061a2b420197011c15638
Frederico Araujo <fred.the.master@gmail.com>
2010-02-28 22:02:05 +0700

    no need to add content to the database as for now

--- app/models/feed_site.rb
+++ app/models/feed_site.rb
@@ -42,8 +42,8 @@ class FeedSite < ActiveRecord::Base
           fi.title = t.title
           fi.url = t.url
           fi.author = t.author
-          fi.summary = t.summary
-          fi.content = t.content
+          #fi.summary = t.summary
+          #fi.content = t.content
           fi.published = t.published
           fi.save
           self.feed_entries << fi
