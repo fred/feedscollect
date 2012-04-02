5714cd5e24da9f0283ab750a0aef89c94fc28a49
Frederico Araujo <fred.the.master@gmail.com>
2012-04-02 15:23:03 +0700

    fixed migration for Postgresql/heroku

--- db/migrate/20100301064726_change_sort_order_type.rb
+++ db/migrate/20100301064726_change_sort_order_type.rb
@@ -1,9 +1,11 @@
 class ChangeSortOrderType < ActiveRecord::Migration
   def self.up
-    change_column(:feed_sites, :sort_order, :integer, :default => 200)
+    remove_column :feed_sites, :sort_order
+    add_column    :feed_sites, :sort_order, :integer, :default => 200
   end
 
   def self.down
-    change_column(:feed_sites, :sort_order, :string, :default => 200)
+    remove_column :feed_sites, :sort_order
+    add_column    :feed_sites, :sort_order, :string
   end
 end
