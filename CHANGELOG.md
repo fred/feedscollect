76561dd7657213afc13a070ebe5f104c5b946893
Frederico Araujo <fred.the.master@gmail.com>
2013-06-08 14:46:18 +0700

    feed_entries in admin, updated Gemfile

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+## May 24, 2013
+
+  - Upgraded to Rails 3.2.13
+  - Updated Gems, twitter-bootstrap, jquery, sidekiq
+  - Fixed celluloid actor.async.run deprectation on scheduler.rb
+  - Increase concurrency to 8 sidekiq treads
+  - Removed /public/assets from Git
+  - [Heroku] Increased Max connections to 10
+  - [Heroku] Compiled slug size: 30.0MB
--- app/admin/feed_entry.rb
+++ app/admin/feed_entry.rb
@@ -1,3 +1,19 @@
 ActiveAdmin.register FeedEntry do
+  index do
+    column :id
+    column :title
+    column :feed_site
+    column :url do |entry|
+      link_to(entry.url.to_s.truncate(30), entry.url) if entry.url
+    end
+    column :author
+    column :summary do |entry|
+      sanitize(entry.summary.to_s).truncate(60) if entry.summary
+    end
+    column :published
+    column :updated_at
+    column :created_at
+    default_actions
+  end
   
 end
